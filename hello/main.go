66ba8e4668b97651e8ca0acb58393953c36785ac
JUN JIE NAN <nanjj@cn.ibm.com>
2018-07-27 15:07:35 +0800

    Added godebug

---
+++ hello/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/sync/errgroup"
+)
+
+func main() {
+	hello()
+}
+
+var Printf = fmt.Printf
+
+func hello() {
+	g := &errgroup.Group{}
+	for i := 0; i < 10; i++ {
+		g.Go(func() error {
+			Printf("goroutine %02d\n", i)
+			return nil
+		})
+	}
+	g.Wait()
+}
