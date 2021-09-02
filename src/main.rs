1deb96caf0ee5bbe04b8b163b6802e9d5c623b5f
JUN JIE NAN <nanjunjie@gmail.com>
2021-09-03 01:28:07 +0800

    Added unit testing

--- src/main.rs
+++ src/main.rs
@@ -2,39 +2,40 @@ use filetime::FileTime;
 use mdbook::{
     book::{BookItem, BookItem::Chapter},
     errors::Error,
-    preprocess::CmdPreprocessor,
+    preprocess::{CmdPreprocessor, PreprocessorContext},
 };
 use serde_json::to_writer;
 use std::{
     fs::{metadata, File},
-    io,
-    io::{BufWriter, Write},
-    path::PathBuf,
+    io::{stdin, stdout, BufWriter, Write},
+    path::Path,
 };
 
-fn handle(section: &mut BookItem) -> Result<(), Error> {
+fn newer_than<P: AsRef<Path>>(p1: P, p2: P) -> Result<bool, Error> {
+    Ok(FileTime::from_last_modification_time(&metadata(p1)?)
+        > FileTime::from_last_modification_time(&metadata(p2)?))
+}
+
+fn write_index_html<P: AsRef<Path>>(index: P, target: &str) -> Result<(), Error> {
+    let file = File::create(index)?;
+    let mut writer = BufWriter::new(&file);
+    write!(
+        &mut writer,
+        "<head><meta http-equiv=\"refresh\" content=\"0;url={}\"></head>",
+        target
+    )?;
+    Ok(())
+}
+
+fn handle(ctx: &PreprocessorContext, section: &mut BookItem) -> Result<(), Error> {
     if let Chapter(ref ch) = *section {
         if ch.name == "Home" {
             if let Some(ref path) = ch.path {
-                let mut cpath = PathBuf::from(path);
-                let mut srcmd = PathBuf::from("src");
-                srcmd.push(cpath.to_str().unwrap());
-                let md1 = metadata(srcmd)?;
-                let md2 = metadata("src/index.html")?;
-                if FileTime::from_last_modification_time(&md1)
-                    <= FileTime::from_last_modification_time(&md2)
-                {
-                    return Ok(());
-                }
-                cpath.set_extension("html");
-                if let Some(ref idxuri) = cpath.to_str() {
-                    eprintln!("Get Home {}", idxuri);
-                    let file = File::create("src/index.html")?;
-                    let mut writer = BufWriter::new(&file);
-                    write!(
-                        &mut writer,
-                        "<head><meta http-equiv=\"refresh\" content=\"0;url={}\"></head>",
-                        idxuri
+                let ref src = ctx.config.book.src;
+                if let Ok(true) = newer_than(src.join(path), src.join("index.html")) {
+                    write_index_html(
+                        src.join("index.html"),
+                        path.with_extension("html").to_str().unwrap(),
                     )?;
                 }
             }
@@ -44,12 +45,44 @@ fn handle(section: &mut BookItem) -> Result<(), Error> {
 }
 
 fn process() -> Result<(), Error> {
-    let (_ctx, mut book) = CmdPreprocessor::parse_input(io::stdin())?;
-    book.for_each_mut(|section: &mut BookItem| handle(section).unwrap());
-    to_writer(io::stdout(), &book)?;
+    let (ctx, mut book) = CmdPreprocessor::parse_input(stdin())?;
+    book.for_each_mut(|section: &mut BookItem| handle(&ctx, section).unwrap());
+    to_writer(stdout(), &book)?;
     Ok(())
 }
 
 fn main() {
     process().unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs::remove_file;
+    use std::io::Read;
+    #[test]
+    fn test_newer_than() {
+        assert!(newer_than(Path::new("not-exists-1"), Path::new("not-exists-2")).is_err());
+        let res = newer_than(Path::new("index.html"), Path::new("index.html"));
+        assert!(res.is_ok());
+        assert_eq!(false, res.unwrap());
+    }
+
+    #[test]
+    fn test_write_index_html() {
+        let index = Path::new("/tmp/index.html");
+        let target = "2021/euler.html";
+        let res = write_index_html(index, target);
+        assert!(res.is_ok());
+        let mut file = File::open(index).unwrap();
+        let mut s = String::new();
+        // file.read_to_string(&mut s).unwrap();
+        let size = Read::read_to_string(&mut file, &mut s).unwrap();
+        assert_eq!(72, size);
+        assert_eq!(
+            "<head><meta http-equiv=\"refresh\" content=\"0;url=2021/euler.html\"></head>",
+            s
+        );
+        remove_file(index).unwrap();
+    }
 }
