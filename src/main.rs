5dc7f4542b9e481b8ba4a869d1d3cffc91987594
JUN JIE NAN <nanjunjie@gmail.com>
2021-09-02 18:47:57 +0800

    Fixed refresh issue

--- src/main.rs
+++ src/main.rs
@@ -2,34 +2,48 @@ use mdbook::{book::BookItem, errors::Error, preprocess::CmdPreprocessor};
 use serde_json;
 
 use std::{
-    fs::File,
+    fs::{metadata, File},
     io,
     io::{BufWriter, Write},
     path::PathBuf,
 };
 
-fn process() -> Result<(), Error> {
-    let (_ctx, mut book) = CmdPreprocessor::parse_input(io::stdin())?;
-    book.for_each_mut(|section: &mut BookItem| {
-        if let BookItem::Chapter(ch) = section {
-            if ch.name == "Home" {
-                if let Some(ref path) = ch.path {
-                    let mut cpath = PathBuf::from(path);
-                    cpath.set_extension("html");
-                    if let Some(ref idxuri) = cpath.to_str() {
-                        let file = File::create("src/index.html").unwrap();
-                        let mut writer = BufWriter::new(&file);
-                        write!(
-                            &mut writer,
-                            "<head><meta http-equiv=\"refresh\" content=\"0;url={}\"></head>",
-                            idxuri
-                        )
-                        .unwrap();
-                    }
+use filetime::FileTime;
+
+fn handle(section: &mut BookItem) -> Result<(), Error> {
+    if let BookItem::Chapter(ref ch) = *section {
+        if ch.name == "Home" {
+            if let Some(ref path) = ch.path {
+                let mut cpath = PathBuf::from(path);
+                let mut srcmd = PathBuf::from("src");
+                srcmd.push(cpath.to_str().unwrap());
+                let md1 = metadata(srcmd)?;
+                let md2 = metadata("src/index.html")?;
+                if FileTime::from_last_modification_time(&md1)
+                    <= FileTime::from_last_modification_time(&md2)
+                {
+                    return Ok(());
+                }
+                cpath.set_extension("html");
+                if let Some(ref idxuri) = cpath.to_str() {
+                    eprintln!("Get Home {}", idxuri);
+                    let file = File::create("src/index.html")?;
+                    let mut writer = BufWriter::new(&file);
+                    write!(
+                        &mut writer,
+                        "<head><meta http-equiv=\"refresh\" content=\"0;url={}\"></head>",
+                        idxuri
+                    )?;
                 }
             }
         }
-    });
+    }
+    Ok(())
+}
+
+fn process() -> Result<(), Error> {
+    let (_ctx, mut book) = CmdPreprocessor::parse_input(io::stdin())?;
+    book.for_each_mut(|section: &mut BookItem| handle(section).unwrap());
     serde_json::to_writer(io::stdout(), &book)?;
     Ok(())
 }
