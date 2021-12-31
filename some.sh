7f7e744a45efe34d158ef409d2edb83173bf5093
JUN JIE NAN <nanjunjie@gmail.com>
2022-01-01 02:01:28 +0800

    Added some section to emacs

--- some.sh
+++ some.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 cd book
-for d in [0-9][0-9][0-9][0-9]; do
+year=$1
+
+if [ "$year" = "" ]; then
+	year=[0-9][0-9][0-9][0-9]
+fi
+
+for d in $year; do
 	echo $d
 	tar Ccfz $d $d.tar.gz .
 	time acurl -Fcontent=@${d}.tar.gz https://pages.sr.ht/publish/nanjj.srht.site/$d
